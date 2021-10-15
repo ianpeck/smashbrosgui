08673dc414372a7b6c3eb53d34ee7fff27b9cc5b
Ian Peck <ianpeck22@gmail.com>
2021-10-15 03:06:06 -0500

    Added potential db class

--- sql.py
+++ sql.py
@@ -18,7 +18,7 @@ password = os.environ.get('awspassword')
 region = "us-east-2a"
 dbname = os.environ.get('awsdb')
 
-# ========= Functions =========n
+# ========= Functions =========
 def query_sql(query):
     connection = pymysql.connect(host=endpoint, database=dbname, user=user, port=port, password=password)
     with connection:
@@ -26,3 +26,26 @@ def query_sql(query):
         cur.execute(query)
         fighter = cur.fetchall()
         return fighter
+
+# ========= Class? ===========
+# class MySQLConnection:
+#     def __init__(self, endpoint, user, password, port, region, dbname):
+#         self.endpoint = endpoint
+#         self.user = user
+#         self.password = password
+#         self.port = port
+#         self.region = region
+#         self.dbname = dbname
+#         self.connection = None
+
+#     def connect(self):
+#         self.connection = pymysql.connect(host=self.endpoint, database=self.dbname, user=self.user, port=self.port, password=self.password)
+#         return self.connection
+    
+#     def query(self, query):
+#         with self.connection:
+#             cur = self.connection.cursor()
+#             data = cur.execute(query)
+#             rows = data.fetchall()
+#             return rows
+
